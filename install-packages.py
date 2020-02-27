09c519f7403e49d59fe9dd977ea4fa85d255e7ef
eropsyren <eropsyren@gmail.com>
2020-02-27 13:00:11 +0100

    add new package file and update install-packages.py script

--- install-packages.py
+++ install-packages.py
@@ -1,8 +1,10 @@
 #!/bin/python3
 
-import os
+import os, sys
 
-with open('packages.txt') as f:
-	for line in f:
-		if line != '\n':
-			os.system(f'sudo apt -y install {line}') 
+for file in sys.argv[1:]:
+    with open(f'{file}') as f:
+            for line in f:
+                    if line != '\n':
+                            os.system(f'sudo apt -y install {line}') 
+
